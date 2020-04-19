332fe0525115b3d2231319a64ef86d035da663c2
Chase McWhirt <mcwhirtc@oregonstate.edu>
2020-04-19 12:22:12 -0700

    #2 Created an empty script file for the vitals renderering.

---
+++ Assets/Scripts/VitalsRenderer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VitalsRenderer : MonoBehaviour {
+    // Start is called before the first frame update
+    public void Start() {
+        
+    }
+
+    // Update is called once per frame
+    public void Update () {
+    }
+}
